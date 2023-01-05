c14db8445bbc50ca1d399812d1e1c0b236d3e540
Nweke Chima Gideon <nweke_chima@yahoo.com>
2023-01-05 10:27:47 -0800

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-def add_0(a, b):
-    a = 1
-    b = 2
-    total = a + b
-    print("1 + 2 =  ", total)
+from add_0 import add
 
-add_0(1, 2)
+a = 1
+b = 2
 
+print("{0} + {1} = {2}".format(a, b, add(a, b)))
